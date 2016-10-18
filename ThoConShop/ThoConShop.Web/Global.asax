2ac752dfc393d7f9c7065c971e392efac30ef66d
Thuan <nnguyen4@amaris.com>
2016-10-18 16:31:29 +0700

    First Commit

---
+++ ThoConShop/ThoConShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThoConShop.Web.MvcApplication" Language="C#" %>
---
+++ ThoConShop/ThoConShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThoConShop.Web.Startup))]
+namespace ThoConShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
