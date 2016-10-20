0b2f9714eb3adf8c47f8a7f274dc8d54af9ad938
Thuan <nnguyen4@amaris.com>
2016-10-20 17:42:58 +0700

    add scroll to top

--- ThoConShop/ThoConShop.Web/Controllers/HomeController.cs
+++ ThoConShop/ThoConShop.Web/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace ThoConShop.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
     }
 }
