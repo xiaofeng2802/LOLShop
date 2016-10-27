77ed2bc5f4e06a8a5acf878a12dfc11df498cced
Thuan <carrie.he1704@gmail.com>
2016-10-27 22:01:54 +0700

    delete ts

--- ThoConShop/ThoConShop.Api/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-﻿//import { NgModule }      from 'angular/core';
-//import { BrowserModule } from 'angular/platform-browser';
-
-//@NgModule({
-//  imports:      [ BrowserModule ]
-//})
-
-//export class AppModule { }
